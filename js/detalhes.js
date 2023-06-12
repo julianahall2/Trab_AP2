16b4b5909206311fc6e4f3ebdb932ce26082947e
Juliana Hall <juju.azeredohall@gmail.com>
2023-06-12 19:01:30 -0300

    terminando

--- js/detalhes.js
+++ js/detalhes.js
@@ -1,17 +1,64 @@
+const container = document.createElement('div');
+container.style.display = 'grid';
+container.style.gridTemplateColumns = '1fr 1fr';
+
+
+
 const div = document.createElement('div');
+div.style.background = '#fff';
+div.style.border = '3px solid #000';
+div.style.borderRadius = '0';
+div.style.boxShadow = '15px 15px 0 -2.5px #fff, 15px 15px 0 0 #000';
+div.style.width = '700px';
+
+div.style.height = '800px';
+
+
 const imagem = document.createElement('img');
 const legenda = document.createElement('h5');
 const descricao = document.createElement('h5');
-
+descricao.innerHTML = localStorage.getItem('descricao');
+descricao.style.color = 'black';
+descricao.style.width = '400px';
+descricao.style.marginLeft = ' 100px';
 imagem.src = localStorage.getItem('caminho');
-imagem.position = 'relative';
+imagem.style.width = '40%';
+imagem.style.height = '60%';
 
 legenda.innerHTML = localStorage.getItem('nome');
-legenda.style.alignItems = 'center';
+legenda.style.textAlign = 'center';
+legenda.style.fontFamily = 'Arial, sans-serif';
+legenda.style.fontSize = '18px';
+legenda.style.color = 'black';
 
-descricao.innerHTML= localStorage.getItem('descricao');
+const divDescricao = document.createElement('div');
+divDescricao.style.display = 'flex';
+divDescricao.style.flexDirection = 'column';
+divDescricao.style.alignItems = 'center';
+divDescricao.style.justifyContent = 'center';
+divDescricao.style.color = '#fff';
 
-div.appendChild(descricao);
-div.appendChild(imagem);
+divDescricao.style.width = '100%';
+divDescricao.style.height = '100%';
+divDescricao.style.fontSize = '20px';
+divDescricao.style.fontWeight = '900';
+
+
+divDescricao.appendChild(imagem);
 div.appendChild(legenda);
-document.body.appendChild(div);
+div.appendChild(descricao);
+document.body.appendChild(divDescricao);
+
+container.appendChild(divDescricao);
+container.appendChild(div);
+
+const voltar = document.createElement('button');
+voltar.textContent = 'Voltar para página inicial';
+voltar.addEventListener('click', function() {
+  window.history.back();
+});
+
+container.appendChild(voltar);
+
+document.body.appendChild(container);
+
