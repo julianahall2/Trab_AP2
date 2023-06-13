accaa364fd67a0a1f33b1905192513c1ecdeefe5
Juliana Hall <juju.azeredohall@gmail.com>
2023-06-13 10:56:04 -0300

    tentando concertar o erro

--- js/script.js
+++ js/script.js
@@ -12,20 +12,6 @@ group.style.maxWidth = '190px';
 group.classList.add('group');
 group.style.flexDirection = 'flex-start'
 
-const svg = document.createElement('svg');
-svg.classList.add('icon');
-svg.setAttribute('aria-hidden', 'true');
-svg.setAttribute('viewBox', '0 0 24 24');
-
-const g = document.createElement('g');
-
-const path = document.createElement('path');
-path.setAttribute('d', 'M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z');
-
-g.appendChild(path);
-svg.appendChild(g);
-group.appendChild(svg);
-
 const input = document.createElement('input');
 input.style.width= '100%';
 input.style.height= '40px';
